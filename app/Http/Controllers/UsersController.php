ee24d992fb452d92559fa3cdd3623e48bfbe613d
choweast <www.apps4s@gmail.com>
2018-04-17 00:13:47 +0800

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,23 +13,30 @@ class UsersController extends Controller
     {
         return view('users.show', compact('user'));
     }
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);// authorize方法接收两个参数，第一个为授权策略的名称，第二个为进行授权验证的数据。
         return view('users.edit', compact('user'));
     }
+
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
-            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 362);
             if ($result) {
                 $data['avatar'] = $result['path'];
             }
         }
 
         $user->update($data);
-        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
+        return redirect()->route('users.show', $user->id)->with('success', '更新成功😊！');
     }
 }
---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    public function update(User $currentUser, User $user)//授权策略文件，update 方法接收两个参数，第一个参数默认为当前登录用户实例，第二个参数则为要进行授权的用户实例。当两个 id 相同时，则代表两个用户是相同用户，用户通过授权，可以接着进行下一个操作。如果 id 不相同的话，将抛出 403 异常信息来拒绝访问。
+    {
+        return $currentUser->id === $user->id;
+    }
+}
