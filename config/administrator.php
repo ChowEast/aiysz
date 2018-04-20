0b776335c2c90c8395a363b9476bbe2bc790fb92
choweast <www.apps4s@gmail.com>
2018-04-20 13:18:41 +0800

    Admin can manage content

--- config/administrator.php
+++ config/administrator.php
@@ -39,7 +39,14 @@ return array(
     'menu' => [
         '用户与权限' => [
             'users',
+            'roles',
+            'permissions',
         ],
+    '内容管理' => [
+        'categories',
+        'topics',
+        'replies',
+    ],
     ],
 
     /*
